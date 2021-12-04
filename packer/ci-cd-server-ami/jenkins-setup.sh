b212e9647696a8624124fc6826a6f55755894e1b
vivek22117 <vivekmishra22117@gmail.com>
2021-12-04 21:13:17 +0530

    updating packer script for CI/CD node and adding Terraform binaries

--- packer/ci-cd-server-ami/jenkins-setup.sh
+++ packer/ci-cd-server-ami/jenkins-setup.sh
@@ -3,9 +3,30 @@
 sudo yum update -y
 
 echo "Install git"
-sudo yum install -y git
-sudo yum install -y wget
-sudo yum install -y unzip
+sudo yum update -y
+sudo yum install wget unzip git -y
+sleep 5
+
+echo "Installing AWS CLI v2"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+
+sudo amazon-linux-extras install -y docker
+systemctl enable docker
+systemctl start docker
+sudo usermod -aG docker ec2-user
+sudo chkconfig docker on
+
+docker --version
+
+
+echo "Install Terraform"
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VER}_linux_amd64.zip
+mv terraform /usr/local/bin/
+terraform version
 
 
 echo "Install Jenkins stable release"
@@ -27,3 +48,6 @@ echo "Configure Jenkins"
 chmod +x /tmp/install-plugins.sh
 bash /tmp/install-plugins.sh
 service jenkins start
+
+
+echo "SUCCESS! Installation succeeded!"
