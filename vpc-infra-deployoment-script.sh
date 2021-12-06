4098a782309d59091b7548c9fbc8d1212088caa9
vivek22117 <vivekmishra22117@gmail.com>
2021-12-06 10:16:10 +0530

    adding basch script to deploy VPC network

---
+++ vpc-infra-deployoment-script.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+
+echo ====================================== Info =======================================================
+echo "The below script will first create application  resources"
+echo -e "===============================================================================================\n\n"
+
+
+echo -e "\n\n =========================== Choose Terraform Execution Type ==========================="
+
+PS3="Select the terraform execution type: "
+
+select EXEC_TYPE in apply destroy
+do
+    echo "You have decided to $EXEC_TYPE the AWS resources!"
+    break
+done
+
+
+
+function terraform_deployment() {
+    echo -e "\n\n==================== Creating VPC Network Resources ========================="
+
+    cd infra-deployment/vpc-network
+
+    terraform init -reconfigure
+    terraform plan -var-file="dev.tfvars" -var="environment=dev"
+    terraform apply -var-file="dev.tfvars" -var="environment=dev" -auto-approve
+
+    cd ../..
+
+    echo -e "========================= Completed ================================================ \n\n"
+}
+
+
+if [ $EXEC_TYPE == 'apply' ]; then
+
+  terraform_deployment
+
+fi
+
+
+if [ $EXEC_TYPE == 'destroy' ]; then
+
+  echo -e "\n\n ============================ Destroying CI/CD Resources ========================="
+  cd infra-deployment/vpc-network
+
+  terraform init -reconfigure
+
+  terraform destroy -var-file="dev.tfvars" -var="environment=dev" -auto-approve
+  cd ../..
+
+  echo -e "========================= Destruction Completed ================================================ \n\n"
+
+fi
