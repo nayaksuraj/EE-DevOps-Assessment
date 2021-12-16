68185a775306c5ea32203abc42e55dc71482ea7f
vivek22117 <vivekmishra22117@gmail.com>
2021-12-16 23:37:50 +0530

    updating plugins file

--- packer/ci-cd-server-ami/jenkins-setup.sh
+++ packer/ci-cd-server-ami/jenkins-setup.sh
@@ -4,15 +4,17 @@
 echo "Install git"
 sudo yum update -y
 sudo yum install wget unzip git -y
+sudo amazon-linux-extras install epel -y
+sudo yum-config-manager --enable epel
 sleep 5
 
 echo "Install Jenkins stable release"
-sudo yum -y install java-1.8.0-openjdk-devel -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins-ci.org/redhat/jenkins.repo --no-check-certificate
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins-ci.org/redhat-stable/jenkins.repo --no-check-certificate
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 yum --showduplicates list jenkins | expand
-sudo yum install -y jenkins-2.289-1.1
+sudo yum install -y jenkins-2.319.1-1.1
 chkconfig jenkins on
 
 
